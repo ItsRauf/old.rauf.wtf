07da058f90607a17123eff6ddde46c890ec58ba5
ItsRauf <raufislam2003@gmail.com>
2020-06-08 14:19:38 -0400

    test

--- guildcount/paramCheck.js
+++ guildcount/paramCheck.js
@@ -1,6 +1,6 @@
 // console.log(window.location.hash);
 if (window.location.hash.length === 0) {
   window.location.replace(
-    "https://discord.com/oauth2/authorize?client_id=447809656652562433&redirect_uri=https%3A%2F%2Frauf.wtf%2Fguildcount%2F&response_type=token&scope=guilds%20identify"
+    "https://discord.com/oauth2/authorize?client_id=447809656652562433&redirect_uri=https%3A%2F%2Frauff.wtf%2Fguildcount%2F&response_type=token&scope=guilds%20identify"
   );
 }
