3b29731b07c1d82b1a91e97518b30f2923196563
Commandtechno <68407783+Commandtechno@users.noreply.github.com>
2021-04-18 20:09:41 -0500

    lol 201 guilds go brr
    in the rare case that somebody breaks discord more than we initially expected they would break discord this should work but im not sure i havent tested it i should probably do that

--- guildcount/index.js
+++ guildcount/index.js
@@ -1,78 +1,67 @@
 const hash = window.location.hash.substr(1);
 const values = hash.split("&").reduce((result, item) => {
-  const parts = item.split("=");
-  result[parts[0]] = parts[1];
-  return result;
+    const parts = item.split("=");
+    result[parts[0]] = parts[1];
+    return result;
 }, {});
 
 history.pushState(
-  "",
-  document.title,
-  window.location.pathname + window.location.search
+    "",
+    document.title,
+    window.location.pathname + window.location.search
 );
 
 async function getData() {
-  const userData = await fetch("https://discord.com/api/v8/users/@me", {
-    method: "GET",
-    mode: "cors",
-    cache: "no-cache",
-    headers: {
-      Authorization: `${values.token_type} ${values.access_token}`,
-    },
-  });
 
-  const user = await userData.json();
-
-  const userGuildData = await fetch(
-    "https://discord.com/api/v8/users/@me/guilds",
-    {
-      method: "GET",
-      mode: "cors",
-      cache: "no-cache",
-      headers: {
-        Authorization: `${values.token_type} ${values.access_token}`,
-      },
-    }
-  );
-
-  user.guilds = await userGuildData.json();
-
-  // in the rare case somebody broke discord
-  if (user.guilds.length === 100) {
-    const after = user.guilds[99].id;
-    const extraGuilds = await fetch(
-      `https://discord.com/api/v8/users/@me/guilds?after=${after}`,
-      {
+    const userData = await fetch("https://discord.com/api/v8/users/@me", {
         method: "GET",
         mode: "cors",
         cache: "no-cache",
         headers: {
-          Authorization: `${values.token_type} ${values.access_token}`,
+            Authorization: `${values.token_type} ${values.access_token}`,
         },
-      }
-    );
-    const extraGuildsJson = await extraGuilds.json();
-    if (extraGuildsJson.length) user.guilds.push(...extraGuildsJson);
-  }
+    });
 
-  return user;
+    const user = await userData.json();
+    user.guilds = await getGuilds(0)
+
+    return user;
+}
+
+async function getGuilds(after) {
+    const guilds = await fetch(
+        `https://discord.com/api/v8/users/@me/guilds?after=${after}`,
+        {
+            method: "GET",
+            mode: "cors",
+            cache: "no-cache",
+            headers: {
+                Authorization: `${values.token_type} ${values.access_token}`,
+            },
+        }
+    );
+
+    let guildsJson = await guilds.json()
+    if (guildsJson.length === 100) guildsJson.concat(await getGuilds(guildsJson[99].id))
+
+    return guildsJson
 }
 
 getData().then((user) => {
-  console.log(user);
-  const avatarRow = document.getElementById("avatar-row");
-  const img = document.createElement("img");
-  img.setAttribute(
-    "src",
-    `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}?size=256`
-  );
-  img.setAttribute("alt", `${user.username}#${user.discriminator}'s Avatar`);
-  img.setAttribute("id", "avatar");
-  avatarRow.appendChild(img);
+    console.log(user);
+    const avatarRow = document.getElementById("avatar-row");
+    const img = document.createElement("img");
+    img.setAttribute(
+        "src",
+        `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}?size=256`
+    );
+    img.setAttribute("alt", `${user.username}#${user.discriminator}'s Avatar`);
+    img.setAttribute("id", "avatar");
+    avatarRow.appendChild(img);
 
-  const userH3 = document.getElementById("user");
-  userH3.innerText = `Hello ${user.username}#${user.discriminator}`;
+    const userH3 = document.getElementById("user");
+    userH3.innerText = `Hello ${user.username}#${user.discriminator}`;
 
-  const countH5 = document.getElementById("count");
-  countH5.innerText = `Your Guild Count is ${user.guilds.length}`;
+    const countH5 = document.getElementById("count");
+    countH5.innerText = `Your Guild Count is ${user.guilds.length}`;
 });
