ac5ed24432b8d0658634d8b6f414b6accfc6cd98
ItsRauf <raufislam2003@gmail.com>
2021-04-23 19:31:10 -0400

    remove slash and guildcount as they are on their own repos

--- slash/dist/helpers/keyofEnum.js
+++
@@ -1,5 +0,0 @@
-export default function keyofEnum(_enum) {
-  const keys = Object.keys(_enum).filter((x) => !(parseInt(x) >= 0));
-  keys.splice(0, 2);
-  return keys;
-}
--- slash/web_modules/react.js
+++
@@ -1,8 +0,0 @@
-import { r as react } from './common/index-df124c49.js';
-export { r as default } from './common/index-df124c49.js';
-
-
-
-var useEffect = react.useEffect;
-var useState = react.useState;
-export { useEffect, useState };
